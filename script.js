0caeb2dfb0108f41dc6f6ca8bd66150bde7f20da
patrikralf <189445915+patrikralf@users.noreply.github.com>
2025-01-15 18:07:58 +0100

    added first function
    create function that returns two strings

---
+++ script.js
@@ -0,0 +1,18 @@
+const rock = ( "rock" );
+const paper = ( "paper" );
+
+let answer = prompt( "Choice" );
+ if( answer === "rock" )
+    alert( "your answer is rock");
+
+ if ( answer === "paper" )
+     alert( "your answer is paper" )
+
+
+/*
+function getComputerChoice() { 
+        return "paper" + " is my choice" 
+
+}
+console.log(getComputerChoice())
+*/
